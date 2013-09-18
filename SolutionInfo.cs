69f9dca09b2a3a87321aa234f3279c2a41ef39de
marcusoft <marcusoft.net@gmail.com>
2013-09-18 10:15:33 +0200

    Updated version number 1.0.2

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
