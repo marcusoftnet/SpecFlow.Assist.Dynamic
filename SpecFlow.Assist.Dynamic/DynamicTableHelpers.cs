7dc0da9f5992b2baffe31df896ce36478808d44b
Marcus Hammarberg <marcusoft.net@gmail.com>
2019-05-17 15:24:08 +0200

    Removed reference to ImpromptuInterface

--- SpecFlow.Assist.Dynamic/DynamicTableHelpers.cs
+++ SpecFlow.Assist.Dynamic/DynamicTableHelpers.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using ImpromptuInterface;
 using Dynamitey;
 using Dynamitey.DynamicObjects;
 
